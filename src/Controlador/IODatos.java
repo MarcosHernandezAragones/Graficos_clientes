78868f2aa2e333dc7de11d7a46ce6ebc32c84701
Marcos <marcoshernandezaragones@gmail.com>
2021-04-14 14:29:48 +0200

    fin IODatos

--- src/Controlador/IODatos.java
+++ src/Controlador/IODatos.java
@@ -6,7 +6,18 @@
 package Controlador;
 
 import Modelo.Cliente;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.stream.FileImageInputStream;
 
 /**
  *
@@ -14,8 +25,64 @@ import java.util.ArrayList;
  */
 public class IODatos {
     
-    public void cargarDatos(ArrayList <Cliente> vclientes){
-    
+    public static void guardarDatos(ArrayList <Cliente> vclientes){
+         //guardar==escribir;
+         
+         String rutaFichero = "Clientes.dat";
+         
+         File f = new File(rutaFichero);
+         
+         if (!f.exists()) {
+             try {
+                 f.createNewFile();
+             } catch (IOException ex) {
+                 Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+             }
+        }
+         
+        try (FileOutputStream fo = new FileOutputStream(f);
+            ObjectOutputStream escribir = new ObjectOutputStream(fo);) {
+            
+            escribir.writeObject(vclientes);
+            
+        } catch (FileNotFoundException ex) {
+          //  Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+          //  Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+            
+    public static ArrayList<Cliente> cargarDatos(){
+         //Cargar==Leer;
+         ArrayList <Cliente> vclientes = new ArrayList<>();
+         String rutaFichero = "Clientes.dat";
+         
+         File f = new File(rutaFichero);
+         
+         if (!f.exists()) {
+             try {
+                 f.createNewFile();
+             } catch (IOException ex) {
+                 Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+             }
+        }
+         
+        try (FileInputStream fi = new FileInputStream(f);
+             ObjectInputStream leer = new ObjectInputStream(fi);) {
+            
+             
+                vclientes = (ArrayList<Cliente>)leer.readObject();
+             
+            
+        } catch (FileNotFoundException ex) {
+            //Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+           // Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+           // Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return vclientes;
     }
     
 }
